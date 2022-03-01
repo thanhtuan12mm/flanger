5373f5a3befc9376648e77ea4686585774ed4002
thanhtuan12mm <thanhtuan12mm@gmail.com>
2022-03-01 21:52:28 +0700

    Remove comment in line_items_controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,25 +3,20 @@ class LineItemsController < ApplicationController
   before_action :set_line_item, only: %i[ show edit update destroy ]
   before_action :set_cart, only: [:create]
 
-  # GET /line_items or /line_items.json
   def index
     @line_items = LineItem.all
   end
 
-  # GET /line_items/1 or /line_items/1.json
   def show
   end
 
-  # GET /line_items/new
   def new
     @line_item = LineItem.new
   end
 
-  # GET /line_items/1/edit
   def edit
   end
 
-  # POST /line_items or /line_items.json
   def create
     instrument = Instrument.find(params[:instrument_id])
     @line_item = @cart.add_instrument(instrument)
@@ -37,7 +32,6 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /line_items/1 or /line_items/1.json
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
