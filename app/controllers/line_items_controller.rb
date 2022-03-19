1bfe1ee708f0f7a038840c1ab528b49c37cd96ba
thanhtuan12mm <thanhtuan12mm@gmail.com>
2022-03-19 21:54:00 +0700

    Controller needless code remove

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -20,48 +20,32 @@ class LineItemsController < ApplicationController
   def create
     instrument = Instrument.find(params[:instrument_id])
     @line_item = @cart.add_instrument(instrument)
-
-    respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Item added to cart." }
-        format.json { render :show, status: :created, location: @line_item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
+    if @line_item.save
+      redirect_to @line_item.cart, notice: "Item added to cart."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
+    if @line_item.update(line_item_params)
+      redirect_to line_item_url(@line_item), notice: "Line item was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /line_items/1 or /line_items/1.json
   def destroy
     @cart = Cart.find(session[:cart_id])
     @line_item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to cart_path(@cart), notice: "Line item was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to cart_path(@cart), notice: "Line item was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_line_item
       @line_item = LineItem.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def line_item_params
       params.require(:line_item).permit(:instrument_id, :cart_id)
     end
